54e08ab8a1e79df5ad7e2c12279e4fb11b93229d
Michael Layzell <michael@redrasor.com>
2014-01-07 19:18:23 -0500

    Changed stuff

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request
-from flask.ext.gears import Gears
-from gears_less import LESSCompiler
+from flask.ext.assets import Environment, Bundle
 
 app = Flask(__name__)
+assets = Environment(app)
 
-gears = Gears(
-    compilers={'.less': LESSCompiler.as_handler()},
-    assets_folder='static',
-)
-gears.init_app(app)
+js = Bundle('js/jquery.js', 'js/jquery.djax.js', 'js/bootstrap.js', 'js/app.js', output='gen/packed.js')
+assets.register('js_all', js)
+
+scss = Bundle('styles/app.scss', filters='scss', output='gen/app.css')
+assets.register('css_all', scss)
+# less = Bundle('styles/global.less', filters='less', output='gen/global.css')
+# assets.register('css_all', less)
 
 @app.route('/')
 def home():
