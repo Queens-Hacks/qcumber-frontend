d4625da00fb5bb5a07da296f2354490bfd71c4da
Michael Layzell <michael@thelayzells.com>
2014-06-25 18:51:01 -0400

    Add textbooks & update course info

--- fill.py
+++ fill.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from elasticsearch import Elasticsearch
 import os
 import json
@@ -11,7 +13,7 @@ def courses(es):
             cd.update(coursedata['extra'])
 
             # Push the item into the elasticsearch
-            es.index(index='qcumber', doc_type='course', id=cd['subject'] + ' ' + cd['number'], body=cd)
+            es.index(index='qcumber', doc_type='course', id='{subject} {number}'.format(**cd), body=cd)
 
 def sections(es):
     sections = os.listdir(os.path.join('out', 'sections'))
@@ -21,7 +23,7 @@ def sections(es):
             sd = sectiondata['basic']
             sd['classes'] = sectiondata['classes']
 
-            es.index(index='qcumber', doc_type='section', id=sd['subject'] + ' ' + sd['class_num'] + ' ' + sd['solus_id'], body=sd)
+            es.index(index='qcumber', doc_type='section', id='{year} {season} {subject} {course} {solus_id}'.format(**sd), body=sd)
 
 def subjects(es):
     subjects = os.listdir(os.path.join('out', 'subjects'))
@@ -30,11 +32,20 @@ def subjects(es):
             subjectdata = json.loads(f.read())
             es.index(index='qcumber', doc_type='subject', id=subjectdata['abbreviation'], body=subjectdata)
 
+def textbooks(es):
+    textbooks = os.listdir(os.path.join('out', 'textbooks'))
+    for textbook in textbooks:
+        with open(os.path.join('out', 'textbooks', textbook)) as f:
+            textbookdata = json.loads(f.read())
+            isbn = textbookdata['isbn_13'] or textbookdata['isbn_10']
+            es.index(index='qcumber', doc_type='textbook', id=isbn, body=textbookdata)
+
 def main() :
     es = Elasticsearch()
-    courses(es)
-    sections(es)
-    subjects(es)
+    #courses(es)
+    #sections(es)
+    #subjects(es)
+    textbooks(es)
     pass
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
+
 from flask import Flask, render_template, abort, redirect, url_for, request
 from elasticsearch import Elasticsearch
+from datetime import datetime
+import dateutil.parser
 import re
 
-BIGNUM=1000 # The maximum number of things which you query for
+BIGNUM=10000 # The maximum number of things which you query for
 
 es = Elasticsearch() # TODO: Set port & shit
 
@@ -11,6 +15,43 @@ app.config.update(
     DEBUG=True,
     PROPAGATE_EXCEPTIONS=True
 )
+
+def group_courses(courses):
+    """
+    Groups courses based on the career. The groups are sorted in reverse
+    alphabetical order. The return value is an array of pairs, where the
+    first element is the Career, and the second is the courses.
+    """
+    grouped = {}
+    for course in courses:
+        old = grouped.get(course['career'], [])
+        old.append(course)
+        grouped[course['career']] = old
+
+    return reversed(sorted(grouped.iteritems(), key=lambda x: x[0]))
+
+@app.template_filter('timeformat')
+def format_time(value):
+    if value:
+        dt = dateutil.parser.parse(value)
+        return dt.strftime("%I:%M%p")
+    else:
+        return ''
+
+@app.template_filter('dayformat')
+def format_day(value):
+    if value:
+        dt = dateutil.parser.parse(value)
+        return dt.strftime("%B %d %Y")
+    else:
+        return 'N/A'
+
+@app.template_filter('dayofweek')
+def format_dow(value):
+    if value:
+        return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][value]
+    else:
+        return 'N/A'
 
 @app.route('/')
 def index():
@@ -45,22 +86,59 @@ def subject(subject):
     }, _source=True, size=BIGNUM)['hits']['hits']
 
     courses = sorted([x['_source'] for x in res], key=lambda x: x['number'])
+    careers = group_courses(courses)
 
-    return render_template('subject.html', subject=sub['_source'], courses=courses)
+    return render_template('subject.html', subject=sub['_source'], careers=careers)
 
 @app.route('/catalog/<subject>/<course>')
 def course(subject, course):
+    # Get the subject
     sub = es.get(index='qcumber', doc_type='subject', id=subject)
     if not sub['found']:
         abort(404)
 
+    # Get the course
     course = es.get(index='qcumber', doc_type='course', id=sub['_source']['abbreviation'] + ' ' + course)
     if not course['found']:
         return abort(404)
 
-    sections = [] # TODO: Implement
+    # Get the textbooks
+    textbooks = es.search(index='qcumber', doc_type='textbook', body={
+        'query': {
+            'match_phrase': {
+                'courses': '{} {}'.format(sub['_source']['abbreviation'], course['_source']['number'])
+            }
+        }
+    }, _source=True, size=BIGNUM)['hits']['hits']
 
-    return render_template('course.html', subject=sub['_source'], course=course['_source'], sections=sections)
+    tbooks = [x['_source'] for x in textbooks]
+
+    # Get the sections
+    sects = es.search(index='qcumber', doc_type='section', body={
+        'query': {
+            'bool': {
+                'must': [
+                    {
+                        'match': { 'course': course['_source']['number'] }
+                    },
+                    {
+                        'match': { 'subject': sub['_source']['abbreviation'] }
+                    }
+                ]
+            }
+        }
+    }, _source=True, size=BIGNUM)['hits']['hits']
+
+    terms = {}
+    for section in sects:
+        old = terms.get('{year} {season}'.format(**(section['_source'])), [])
+        old.append(section['_source'])
+        terms['{year} {season}'.format(**(section['_source']))] = old
+
+
+    # sections = [x['_source'] for x in sects]
+
+    return render_template('course.html', subject=sub['_source'], course=course['_source'], terms=terms, textbooks=tbooks)
 
 subjectre = r'^[A-Za-z]{2,4}$'
 coursere = r'^[A-Za-z]{2,2} [A-Za-z0-9]{2,4}$'
@@ -72,9 +150,10 @@ def search():
 
     results = es.search(index='qcumber', doc_type='course', body={
         'query': {
-            'fuzzy_like_this': {
-                'fields': ['title', 'number', 'description'],
-                'like_text': query.strip()
+            'query_string': {
+                'query': query.strip(),
+                'lenient': True,
+                'default_operator': 'AND'
             }
         }
     }, _source=True, size=100)['hits']['hits']
@@ -82,14 +161,6 @@ def search():
     reses = [x['_source'] for x in results]
 
     return render_template('search.html', query=query, results=reses)
-
-#@app.route('/search')
-#def search():
-    #query = request.args.get('q', '')
-    #if query.strip() == '':
-        #return render_template('search.html', results=[], query=query)
-    #else:
-        #es.get(index='qcumber', doc_type='')
 
 if __name__ == '__main__':
     app.run(port=3000)
